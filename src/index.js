46666362d1fbcb7754d4205c6290614d0453f558
gtycbjyth <kovaltru@gmail.com>
2021-03-13 13:45:27 +0300

    feat: check()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let test = bracketsConfig.map((elem)=> {
+        return elem[0] + elem[1];
+    })
+    let open = [];
+    for (const i of [...str]) {
+        open.push(i);
+        let contrBr = open[open.length - 2] + open[open.length - 1];
+        if (test.indexOf(contrBr) !== -1) open.length = open.length - 2;
+    }
+    return open.length === 0;
 }
